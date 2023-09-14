df9e9b5547cb2665816591a0c4624d6c18612ec9
pcology <ssgkp.singh@gmail.com>
2023-09-14 09:52:27 +0530

    stringSearch

---
+++ Searching Algorithms/stringSearch.js
@@ -0,0 +1,15 @@
+function stringSearch(str, val) {
+    let count = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < val.length; j++) {
+            if (str[i + j] !== val[j]) {
+                break
+            }
+            console.log(str[i + j], "===", val[j], j)
+            if (j === val.length - 1) count++
+        }
+    }
+    return count
+}
+console.log(stringSearch("ffdgdffs fdffsdf", "ff"))
+
