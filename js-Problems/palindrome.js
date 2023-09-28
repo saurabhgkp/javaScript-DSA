5f598de1040346d1426dc6a2de8a718b67cfb30d
pcology <ssgkp.singh@gmail.com>
2023-09-28 09:42:17 +0530

    Palindrome Number

--- js-Problems/palindrome.js
+++ js-Problems/palindrome.js
@@ -12,3 +12,27 @@ function palindrome(str) {
 }
 
 console.log(palindrome("racecar"))
+
+
+// Palindrome Number 
+function Palindrome(number) {
+    var remainder, temp, reversedNumber = 0;
+    temp = number;
+
+    // our reverse logic for numbers
+    while (number > 0) {
+        remainder = number % 10;
+        number = parseInt(number / 10);
+        reversedNumber = reversedNumber * 10 + remainder;
+    }
+    if (temp === reversedNumber) {
+
+        return true
+    }
+    else {
+        return false
+    }
+
+}
+
+console.log(Palindrome(121))
