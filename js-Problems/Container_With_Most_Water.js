e33bf539549a011537bcb52672ebd8cf5583d540
pcology <ssgkp.singh@gmail.com>
2023-10-02 16:04:32 +0530

    Container With Most Water

---
+++ js-Problems/Container_With_Most_Water.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/container-with-most-water/submissions/
+
+var maxArea = function (height) {
+    let maxArea = 0
+    let start = 0
+    let end = height.length - 1
+    while (start < end) {
+        let currentArea = Math.min(height[start], height[end]) * (end - start)
+        maxArea = Math.max(maxArea, currentArea)
+
+        if (height[start] < height[end]) {
+            start++
+        }
+        else {
+            end--
+        }
+
+        return maxArea
+
+    }
+
+
+
+}
