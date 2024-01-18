689947001829b9d326cd700b19dde581b5ebc413
pcology <ssgkp.singh@gmail.com>
2024-01-19 00:19:02 +0530

    stringSearch

--- String/05_string_Search.js
+++ String/05_string_Search.js
@@ -1,3 +1,20 @@
+function stringSearch(long, short) {
+    //best case
+    if (long === short) return 1
+    let count = 0
+    // outer loop  inner loop
+    for (let i = 0; i < long.length; i++) {
+        for (let j = 0; j < short.length; j++) {
+            if (long[i + j] !== short[j]) break;
+            if (short.length - 1 === j) count++
+        }
+    }
+    return count
+}
+
+console.log(stringSearch("lol dfk lod lol", "lol"))
+
+
 // string search
 function stringSearch(strs, match) {
     if (strs === match) return 1
